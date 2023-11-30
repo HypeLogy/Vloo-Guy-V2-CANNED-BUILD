b84bb905198dd9777c105de8840cb307589c03ff
FixedData <99661153+FixedData@users.noreply.github.com>
2023-11-29 23:00:38 -0700

    k vloo additoin

--- example_mods/stages/guy.lua
+++ example_mods/stages/guy.lua
@@ -3,21 +3,54 @@ function onCreate()
 	scaleObject('a', 1, 1);
 	updateHitbox('a');
 	setProperty('a.antialiasing', false);
-      setObjectOrder('a', 0)
+    setObjectOrder('a', 0)
 	addLuaSprite('a',false);
 
 	makeLuaSprite('b','stages/guy/guyfront', 0, 0);
 	scaleObject('b', 1, 1);
 	updateHitbox('b');
 	setProperty('b.antialiasing', false);
-      setObjectOrder('b', 1)
+    setObjectOrder('b', 1)
 	addLuaSprite('b',false);
 
 	makeLuaSprite('c','stages/guy/kosejumpscare', 0, 0);
 	scaleObject('c', 1, 1);
 	updateHitbox('c');
 	setProperty('c.antialiasing', false);
-      setProperty('c.alpha', 0)
-      setObjectOrder('c', 2)
+    setProperty('c.alpha', 0)
+    setObjectOrder('c', 2)
 	addLuaSprite('c',false);
+
+
+	makeAnimatedLuaSprite("vloo", 'stages/guy/Vloofriend',-9,-66)
+	addAnimationByPrefix("vloo", "i", "Vloo guy", 12, false)
+	setProperty('vloo.antialiasing', false)
+	--playAnim("vloo", "i")
+	addLuaSprite('vloo',true)
+	scaleObject('vloo',0.8,0.8,true)
+	setProperty('vloo.alpha', 0)
+end
+
+checkforanim = false
+
+function onEvent(r, r1, r2)
+	if r == '' and r1 == 'fixvloo' then
+		checkforanim = true
+		setProperty('vloo.alpha', 1)
+		setProperty('dad.alpha', 0)
+		playAnim("vloo", "i")
+
+		
+	end
+	
+end
+
+function onUpdatePost(elapsed)
+	if checkforanim then
+		if getProperty('vloo.animation.curAnim.finished') then
+			setProperty('vloo.alpha', 0)
+			setProperty('dad.alpha', 1)
+			checkforanim = false
+		end
+	end
 end
